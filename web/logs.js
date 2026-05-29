849147548108900499d8b2c6ac2064d95debedec
vinikjkkj <viniwarrior4@gmail.com>
2026-05-28 22:13:16 -0300

    update logs script

--- web/logs.js
+++ web/logs.js
@@ -1,9 +1,27 @@
 if (!window.logBack) {
-    window.logBack = require("WALogger").LOG
+    window.logBack = {}
+    for (const level of ['LOG', 'INFO', 'WARN', 'ERROR', 'DEBUG', 'TRACE']) {
+        const fn = require('WALogger')[level]
+        if (typeof fn === 'function') {
+            window.logBack[level] = fn
+        }
+    }
 }
-  
-require("WALogger").LOG =(...args) => {
-    const result = window.logBack(...args)
-    console.log('\u001B[31m[LOG]\u001B[0m', ...args)
-    return result
+
+const COLORS = {
+    LOG: '\u001B[36m', // cyan
+    INFO: '\u001B[34m', // blue
+    WARN: '\u001B[33m', // yellow
+    ERROR: '\u001B[31m', // red
+    DEBUG: '\u001B[90m', // grey
+    TRACE: '\u001B[90m' // grey
+}
+const RESET = '\u001B[0m'
+
+for (const level of Object.keys(window.logBack)) {
+    require('WALogger')[level] = (...args) => {
+        const result = window.logBack[level](...args)
+        console.log(`${COLORS[level] || ''}[${level}]${RESET}`, ...args)
+        return result
+    }
 }
